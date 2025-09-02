9176d8fa143f0a0266adfc34c4ff0fbd5c6dfef4
ShadowCommander <10494922+ShadowCommander@users.noreply.github.com>
2025-09-01 22:39:31 -0700

    Fix reading p2p packet sender_id

--- steam_network.gd
+++ steam_network.gd
@@ -361,7 +361,7 @@ func _read_p2p_packet(packet_size:int):
 		push_warning("Steam Networking: read an empty packet with non-zero size!")
 
 	# Get the remote user's ID
-	var sender_id: int = packet["steam_id_remote"]
+	var sender_id: int = packet["remote_steam_id"]
 	var packet_data: PackedByteArray = packet["data"]
 
 	_handle_packet(sender_id, packet_data)
